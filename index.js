552078ee9914ece2d99ccb9d063a4d186500b6b9
Fumika Mikami <fumicha.3fan1@gmail.com>
2024-11-27 15:13:50 +0000

    studentROutes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const dotenv = require('dotenv') //taking from .env
 dotenv.config() //taking from .env
-const Port = 8000
+const Port = 8001
 const cors = require('cors')
 const connection = require('./db/connection')
 const userRoutes = require('./routes/userRoutes')
