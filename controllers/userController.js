477fd1374b32d370ca117cc118df68fc6c67a46a
Fumika Mikami <fumicha.3fan1@gmail.com>
2025-09-03 07:27:20 +0100

    signin is updated

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const User = require('../model/userModel')
 const Student = require('../model/studentModel')
-
+const bcrypt = require('bcryptjs')
+const sharp = require('sharp')
 
 const signUp = async (req, res) => {
   try {
